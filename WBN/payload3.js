0a7478a5fd991f9ac511bd9f2b9c102252ba305a
davilone32 <126024362+davilone32rl@users.noreply.github.com>
2024-11-04 22:21:23 +0100

    Final

--- WBN/payload3.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const buffer = new ArrayBuffer(64);
-    const view = new DataView(buffer);
-
-    for (let i = 0; i < view.byteLength; i += 4) {
-        view.setUint32(i, 0x41414141 + i);
-        console.log(`Escrito en posición ${i}: ${view.getUint32(i).toString(16)}`);
-    }
-})();
