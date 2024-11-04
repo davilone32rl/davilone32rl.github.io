86dc4f1cef88cac2a33e7284aba9efd7878ef593
davilone32 <126024362+davilone32rl@users.noreply.github.com>
2024-11-04 17:10:03 +0100

    End

---
+++ script.js
@@ -0,0 +1,15 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('kickButton').onclick = function() {
+        window.open('https://www.kick.com/davilone32', '_blank');
+    };
+    document.getElementById('discordButton').onclick = function() {
+        window.open('https://discord.gg/SW3KCNeyKk', '_blank'); 
+    };
+    document.getElementById('youtubeButton').onclick = function() {
+        window.open('https://www.youtube.com/@davilone322', '_blank');
+    };
+    document.getElementById('twitchButton').onclick = function() {
+        window.open('https://www.twitch.tv/davilone322', '_blank');
+    };
+    
+});
