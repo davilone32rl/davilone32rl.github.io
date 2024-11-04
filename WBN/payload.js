25797ff8b868bba5cc886ffd3d8a97b9a30aa335
davilone32 <126024362+davilone32rl@users.noreply.github.com>
2024-11-04 19:52:48 +0100

    not load bin

--- WBN/payload.js
+++ WBN/payload.js
@@ -4,22 +4,11 @@
     const sprayCount = 50;       // Número de objetos para llenar la memoria
 
     // Crear el heap spraying
-    console.log("Iniciando heap spraying agresivo...");
+    console.log("Iniciando heap spraying...");
     for (let i = 0; i < sprayCount; i++) {
         let sprayStr = "A".repeat(spraySize); // Crear una cadena de 10MB
         sprayArray.push(sprayStr);
     }
 
     console.log("Heap spraying completado. Se han colocado objetos grandes en memoria.");
-
-    // Aquí cargarías el código binario
-    fetch('ftp.bin')
-        .then(response => response.arrayBuffer())
-        .then(buffer => {
-
-            alert("Código binario cargado en memoria.");
-        })
-        .catch(error => {
-            alert('Error al cargar el archivo binario:', error);
-        });
 })();
